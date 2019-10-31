2a8cfb6baf6cdbe2228998aff07ff9086eeac3d0
Denis <fixprogram@mail.ru>
2019-10-31 22:14:33 +0300

    Playterest 1.0: was added log in thought the Steam

--- src/app.js
+++ src/app.js
@@ -2,18 +2,58 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = process.env.PORT || 3000 // Подключаться по этому хосту.. На 3010 сервак
+const port = process.env.PORT || 2000 // Подключаться по этому хосту.. На 3010 сервак
 const path = require('path')
 const hbs = require('hbs')
+
+const session = require('express-session')
+const passport = require('passport')
+const SteamStrategy = require('../lib/passport-steam').Strategy
 
 const views = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 const publicDirectoryPath = path.join(__dirname, '../templates/assets') 
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new SteamStrategy({
+  returnURL: 'http://localhost:2000/auth/steam/return',
+  realm: 'http://localhost:2000/',
+  apiKey: 'CFB0BB3EDA8D5FD2342384380B442CC9'
+},
+function(identifier, profile, done) {
+  process.nextTick(function () {
+    profile.identifier = identifier;
+    return done(null, profile);
+  });
+}
+));
+
 app.set('view engine', 'hbs')
 app.set('views', views)
 hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
+
+app.use(session({
+  secret: 'your secret',
+  name: 'name of session id',
+  resave: true,
+  saveUninitialized: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+  secret: 'your secret',
+  name: 'name of session id',
+  resave: true,
+  saveUninitialized: true}));
 
 const mongodb = require('mongodb').MongoClient
 const connectionUrl = 'mongodb://127.0.0.1:27017'
@@ -39,9 +79,32 @@ app.get('/', function(req, res) {
     content.forEach((item) => {
       messages += item.text + ', '
     })
-    res.render('index', {data: messages, tag: game})
+    console.log(req.user)
+    res.render('index', {data: messages, tag: game, user: req.user })
   })
+  
 });
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user, avatar: req.user.photos[2].value });
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/auth/steam',
+  passport.authenticate('steam', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
@@ -67,3 +130,8 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
