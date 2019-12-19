50795dd3e9c140377f204f29f1386699a5d23272
Cantas <fixprogram@mail.ru>
2019-12-19 23:48:09 +0300

    Was changed a port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 2000; // Подключаться по этому хосту.. На 3010 сервак
+const port = process.env.PORT || 3000; // Подключаться по этому хосту.. На 3010 сервак
 const path = require('path');
 const hbs = require('hbs');
 
