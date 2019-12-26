9b0d2593eb4270a7508887fca82900632a385d9a
Cantas <fixprogram@mail.ru>
2019-12-26 21:29:34 +0300

    Was added an authentification

---
+++ api.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+let db = mongoose.connect('mongodb://heroku_969m2gr9:d0ljj3k0df4v7psa45cn26u376@ds129098.mlab.com:29098/heroku_969m2gr9');
+let User = require('models/User');
+
+exports.createUser = function(userData) {
+    let user = {
+        name: userData.name,
+        email: userData.email,
+        password: hash(userData.password)
+    };
+    return new User(user).save()
+};
+
+exports.getUser = function(id) {
+    return User.findOne(id)
+};
+
+exports.checkUser = function(userData) {
+    return User.findOne({email:userData.email}).then(function(doc) {
+        if ( doc.password == hash(userData.password) ) {
+            console.log('The password is okay');
+            return Promise.resolve(doc)
+        } else {
+            return Promise.reject('Reject Wrong')
+        }
+    })
+};
+
+function hash(text) {
+    return crypto.createHash('sha1').update(text).digest('base64')
+}
--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000; // Подключаться по этому хосту.. На 3010 сервак
 const path = require('path');
 const hbs = require('hbs');
+const api = require('./api.js');
 
 const db = require('./db.js');
 db.getCollection(app);
 
+const mongoose = require('mongoose');
 const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
 
 const passport = require('./authentification').getPassport();
 
@@ -20,81 +24,128 @@ const publicDirectoryPath = path.join(__dirname, 'templates/assets');
 app.set('view engine', 'hbs');
 app.set('views', views);
 hbs.registerPartials(partialsPath);
-app.use( express.static(publicDirectoryPath) );
+app.use(express.static(publicDirectoryPath));
 
 app.use(session({
-  secret: 'your secret',
-  name: 'name of session id',
-  resave: true,
-  saveUninitialized: true}) );
+        secret: 'your secret',
+        name: 'name of session id',
+        resave: false,
+        saveUninitialized: false,
+
+        store: new MongoStore({
+            url: 'mongodb://heroku_969m2gr9:d0ljj3k0df4v7psa45cn26u376@ds129098.mlab.com:29098/heroku_969m2gr9'
+        })
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(session({
-  secret: 'your secret',
-  name: 'name of session id',
-  resave: true,
-  saveUninitialized: true}) );
+    secret: 'your secret',
+    name: 'name of session id',
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.get('/', function(req, res) {
-  let collection = app.locals.collection;
-  let game = req.query.game;
+// Create a user
 
-  console.log(req.query.game);
+router.post('/login', function (req, res, next) {
+    if (req.session.user) return res.redirect('/');
 
-  if(game === undefined) game = 'index';
+    api.checkUser(req.body).then(function (user) {
+        if (user) {
+            req.session.user = {id: user._id, name: user.name};
+            res.redirect('/')
+        } else {
+            return next(error)
+        }
+    })
+        .catch(function (error) {
+            return next(error)
+        })
+});
 
-  collection.find({tag: game}).toArray( (error, content) => {
-    if(error) return console.log(error);
+router.post('/', function (req, res, body) {
+    api.createUser(req.body).then(function (result) {
+        console.log("User created");
+    })
+        .catch(function (err) {
+        if (err.toJSON().code == 11000) {
+            res.status(500).send("This email is already exists");
+        }
+    })
+});
 
-    let messages = [];
+router.post('/logout', function(req, res, next) {
+    if (req.session.user) {
+        delete req.session.user;
+        res.redirect('/');
+    }
+});
 
-    content.forEach((item) => {
-      messages += item.text + ', ';
+module.exports = router;
+
+//
+
+app.get('/', function (req, res) {
+    let collection = app.locals.collection;
+    let game = req.query.game;
+
+    console.log(req.query.game);
+
+    if (game === undefined) game = 'index';
+
+    collection.find({tag: game}).toArray((error, content) => {
+        if (error) return console.log(error);
+
+        let messages = [];
+
+        content.forEach((item) => {
+            messages += item.text + ', ';
+        });
+
+        console.log(req.user);
+        res.render('index', {data: messages, tag: game, user: req.user});
     });
-
-    console.log(req.user);
-    res.render('index', {data: messages, tag: game, user: req.user });
-  });
 });
 
-app.get('/account', ensureAuthenticated, function(req, res) {
-  res.render('account', { user: req.user, avatar: req.user.photos[2].value });
+app.get('/account', ensureAuthenticated, function (req, res) {
+    res.render('account', {user: req.user, avatar: req.user.photos[2].value});
 });
 
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 app.get('/auth/steam',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+    passport.authenticate('steam', {failureRedirect: '/'}),
+    function (req, res) {
+        res.redirect('/');
+    });
 
 app.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+    passport.authenticate('steam', {failureRedirect: '/'}),
+    function (req, res) {
+        res.redirect('/');
+    });
 
-io.on('connection', function(socket) {
-  socket.on('chat message', function(msg) {
-    console.log(msg);
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        console.log(msg);
 
-    db.writeMessage(msg.tag, msg.msg);
+        db.writeMessage(msg.tag, msg.msg);
 
-    socket.emit('chat message', msg);
-  });
+        socket.emit('chat message', msg);
+    });
 });
 
-http.listen(port, function() {
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
 
 function ensureAuthenticated(req, res, next) {
-  if ( req.isAuthenticated() ) return next(); 
-  res.redirect('/');
+    if (req.isAuthenticated()) return next();
+    res.redirect('/');
 }
