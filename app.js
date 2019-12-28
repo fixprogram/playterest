3563657ce2d9a6aa21b2d3f1f853b1c164a42550
Cantas <fixprogram@mail.ru>
2019-12-28 18:43:31 +0300

    Was added a bg img

--- app.js
+++ app.js
@@ -156,3 +156,34 @@ function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect('/');
 }
+
+
+// Parsing images
+
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+//подключение ссылки
+request('https://myappest.herokuapp.com/', (error, response, body) => {
+    //если нет ошибки и сервер возвращает код 200
+    if (!error && response.statusCode === 200) {
+
+        // загружаем тело страницы в Cheerio
+        const $ = cheerio.load(body);
+        const srcs = [];
+
+        // указываем класс изображений и откуда их брать
+        $('.fig img', '.img')
+            .each((idx, pic) => {
+                const src = $(pic).attr('src');
+                srcs.push(src)
+            });
+
+
+        srcs.forEach((s, i) => {
+            request(s).pipe(fs.createWriteStream(`pictures/${i}.jpg`))
+        })
+
+    }
+});
