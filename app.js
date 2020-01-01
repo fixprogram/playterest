a9debe31593b60477463c195e991335db5187d50
Cantas <fixprogram@mail.ru>
2020-01-01 20:51:29 +0300

    The parser was added inside

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 3000; // Подключаться по этом
 const path = require('path');
 const hbs = require('hbs');
 const api = require('./api.js');
-const parser = require('./parser.js');
+// const parser = require('./parser.js');
+
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 const db = require('./db.js');
 db.getCollection(app);
@@ -143,7 +147,28 @@ app.get('/register', function (req, res) {
     res.render('register');
 });
 
-parser.imgParse();
+request('https://store.steampowered.com/', (error, response, body) => {
+    //если нет ошибки и сервер возвращает код 200
+    if (!error && response.statusCode === 200) {
+
+        // загружаем тело страницы в Cheerio
+        const $ = cheerio.load(body);
+        const srcs = [];
+
+        // указываем класс изображений и откуда их брать
+        $('.sale_capsule_image_ctn img', '.home_topsellers_ctn')
+            .each((idx, pic) => {
+                const src = $(pic).attr('src');
+                srcs.push(src)
+            });
+
+
+        srcs.forEach((s, i) => {
+            request(s).pipe(fs.createWriteStream('/assets/img/steam/${i}.jpg'));
+        })
+
+    }
+});
 
 io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
