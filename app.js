9952f96ea7e3646d7ba926deaea5a59163d4680e
Cantas <fixprogram@mail.ru>
2020-01-06 14:04:43 +0300

    Were added console logs

--- app.js
+++ app.js
@@ -221,6 +221,7 @@ io.on('connection', function (socket) {
         users.push(nick);
         socket.nick=nick;
         idsnicks[nick]=socket.id;
+        console.log(users);
         io.emit('userlist', users);
     });
 
