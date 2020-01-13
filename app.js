adbbf9b28168446cbacbe7acdff353a5635a81a8
Cantas <fixprogram@mail.ru>
2020-01-13 19:25:25 +0300

    1

--- app.js
+++ app.js
@@ -176,7 +176,8 @@ app.get('/home', function (req, res) {
     if (req.session.user) {
         console.log('id: ' + req.session.user.name);
         let user = api.getUser(req.session.user.name).then(function(user) {
-          console.log(user);
+          console.log('user: ' + user);
+          return user;
         });
         console.log(user.games);
         res.render('home', {userName: req.session.user.name, userID: req.session.user.id, gamesList: user.games, searchInfo});
