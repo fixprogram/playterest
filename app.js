c40989c7f2fd78be6f3e2f072c4cf369f9abee13
Cantas <fixprogram@mail.ru>
2020-01-13 19:35:29 +0300

    Output games

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ app.get('/home', function (req, res) {
         // });
         // console.log('user: ' + user.games);
         console.log('user' + req.session.user);
-        res.render('home', {userName: req.session.user.name, userID: req.session.user.id, gamesList: user.games, searchInfo});
+        res.render('home', {userName: req.session.user.name, userID: req.session.user.id, gamesList: req.session.user.games, searchInfo});
     } else {
         res.render('home', {user: false});
     }
